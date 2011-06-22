dd74991e9aca4dfdb0226566d9d51b01b18134db
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2011-06-22 17:03:00 +0000

    added: get_num_tokens function
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@4 c8a82860-ae11-f3b5-05a3-c0c726401f9a

---
+++ common_schema/routines/text/get_num_tokens.sql
@@ -0,0 +1,24 @@
+-- 
+-- Return number of tokens in delimited text
+-- txt: input string
+-- delimiter: char or text by which to split txt
+--
+-- example:
+--
+-- SELECT get_num_tokens('the quick brown fox', ' ') AS num_token;
+-- Returns: 4
+-- 
+DELIMITER $$
+
+DROP FUNCTION IF EXISTS get_num_tokens $$
+CREATE FUNCTION get_num_tokens(txt TEXT CHARSET utf8, delimiter VARCHAR(255) CHARSET utf8) RETURNS INT UNSIGNED 
+DETERMINISTIC
+NO SQL
+SQL SECURITY INVOKER
+COMMENT 'Return number of tokens in delimited text'
+
+BEGIN
+  RETURN CHAR_LENGTH(txt) - CHAR_LENGTH(REPLACE(txt, delimiter, '')) + 1;
+END $$
+
+DELIMITER ;
