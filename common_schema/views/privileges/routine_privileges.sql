cb868e1b4c58eb0f7744629054755fc8a5185622
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2011-06-23 10:54:15 +0000

    routine_privileges: simplified query
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@6 c8a82860-ae11-f3b5-05a3-c0c726401f9a

--- common_schema/views/privileges/routine_privileges.sql
+++ common_schema/views/privileges/routine_privileges.sql
@@ -1,19 +1,4 @@
 -- 
--- (Internal use): grantees with grant option on routines   
--- 
-CREATE OR REPLACE
-ALGORITHM = TEMPTABLE
-SQL SECURITY INVOKER
-VIEW _grantable_procs_priv AS
-  SELECT 
-    DISTINCT User, Host, Db, Routine_name
-  FROM
-    mysql.procs_priv
-  WHERE 
-     find_in_set('Grant', Proc_priv) > 0
-;
-
--- 
 -- INFORMATION_SCHEMA-like privileges on routines    
 -- 
 CREATE OR REPLACE
@@ -27,11 +12,10 @@ VIEW routine_privileges AS
     Routine_name AS ROUTINE_NAME,
     Routine_type AS ROUTINE_TYPE,
     UPPER(SUBSTRING_INDEX(SUBSTRING_INDEX(Proc_priv, ',', n+1), ',', -1)) AS PRIVILEGE_TYPE,
-    IF(_grantable_procs_priv.User IS NULL, 'NO', 'YES') AS IS_GRANTABLE
+    IF(find_in_set('Grant', Proc_priv) > 0, 'YES', 'NO') AS IS_GRANTABLE
   FROM
     mysql.procs_priv
     CROSS JOIN numbers
-    LEFT JOIN _grantable_procs_priv USING (User, Host, Db, Routine_name)
   WHERE
     numbers.n BETWEEN 0 AND CHAR_LENGTH(Proc_priv) - CHAR_LENGTH(REPLACE(Proc_priv, ',', ''))
     AND UPPER(SUBSTRING_INDEX(SUBSTRING_INDEX(Proc_priv, ',', n+1), ',', -1)) != 'GRANT'
