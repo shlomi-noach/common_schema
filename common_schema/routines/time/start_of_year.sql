72fa46208fcd6b2c744af9be62abea33013f47c5
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2011-07-04 05:36:19 +0000

    Fixed start_of_year, start_of_quarter (were swapped)
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@38 c8a82860-ae11-f3b5-05a3-c0c726401f9a

--- common_schema/routines/time/start_of_year.sql
+++ common_schema/routines/time/start_of_year.sql
@@ -1,23 +1,23 @@
 -- 
--- Returns first day of quarter of given datetime, as DATE object
+-- Returns first day of year of given datetime, as DATE object
 -- 
 -- Example:
 --
--- SELECT start_of_quarter('2010-08-24 11:13:42');
--- Returns: '2010-07-01' (as DATE)
+-- SELECT start_of_year('2011-03-24 11:13:42');
+-- Returns: '2011-01-01' (as DATE)
 --
 
 DELIMITER $$
 
-DROP FUNCTION IF EXISTS start_of_quarter $$
-CREATE FUNCTION start_of_quarter(dt DATETIME) RETURNS DATE 
+DROP FUNCTION IF EXISTS start_of_year $$
+CREATE FUNCTION start_of_year(dt DATETIME) RETURNS DATE 
 DETERMINISTIC
 NO SQL
 SQL SECURITY INVOKER
-COMMENT 'Returns first day of quarter of given datetime, as DATE object'
+COMMENT 'Returns first day of month of given datetime, as DATE object'
 
 BEGIN
-  RETURN DATE(dt) - INTERVAL (MONTH(dt) -1) MONTH - INTERVAL (DAYOFMONTH(dt) - 1) DAY + INTERVAL (QUARTER(dt) - 1) QUARTER;
+  RETURN DATE(dt) - INTERVAL (MONTH(dt) -1) MONTH - INTERVAL (DAYOFMONTH(dt) - 1) DAY;
 END $$
 
 DELIMITER ;
