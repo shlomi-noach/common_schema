b8e0041a372fd1a21303c66ccdfce7448c37b94c
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2011-08-07 07:18:18 +0000

    match_grantee: now dependent-free
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@52 c8a82860-ae11-f3b5-05a3-c0c726401f9a

---
+++ common_schema/routines/privileges/match_grantee.sql
@@ -0,0 +1,59 @@
+-- 
+-- Match an existing account based on user+host
+--
+-- Example:
+--
+-- SELECT mysql_grantee('web_user', '192.128.0.%');
+-- Returns (text): 'web_user'@'192.128.0.%'
+-- 
+DELIMITER $$
+
+DROP FUNCTION IF EXISTS match_grantee $$
+CREATE FUNCTION match_grantee(connection_user char(16) CHARSET utf8, connection_host char(60) CHARSET utf8) RETURNS VARCHAR(100) CHARSET utf8 
+DETERMINISTIC
+READS SQL DATA
+SQL SECURITY INVOKER
+COMMENT 'Match an account based on user+host'
+
+BEGIN
+  DECLARE grantee_user char(16) CHARSET utf8 DEFAULT NULL;
+  DECLARE grantee_host char(60) CHARSET utf8 DEFAULT NULL;
+
+  SELECT
+    MAX(user), MAX(host)
+  FROM (
+    SELECT
+      user, host
+    FROM
+      mysql.user
+    WHERE
+      connection_user RLIKE
+        CONCAT('^',
+          REPLACE(
+            user,
+            '%', '.*'),
+          '$')
+      AND SUBSTRING_INDEX(connection_host, ':', 1) RLIKE
+        CONCAT('^',
+          REPLACE(
+          REPLACE(
+            host,
+            '.', '\\.'),
+            '%', '.*'),
+          '$')
+    ORDER BY
+      CHAR_LENGTH(host) - CHAR_LENGTH(REPLACE(host, '%', '')) ASC,
+      CHAR_LENGTH(host) - CHAR_LENGTH(REPLACE(host, '.', '')) DESC,
+      host ASC,
+      CHAR_LENGTH(user) - CHAR_LENGTH(REPLACE(user, '%', '')) ASC,
+      user ASC
+    LIMIT 1
+  ) select_matching_account
+  INTO 
+    grantee_user, grantee_host;
+    
+    
+  RETURN CONCAT('''', grantee_user, '''@''', grantee_host, '''');
+END $$
+
+DELIMITER ;
