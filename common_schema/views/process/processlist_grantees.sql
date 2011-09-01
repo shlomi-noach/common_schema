5e6b5ea506ed801b5300815b49d0425c85c4e500
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2011-09-01 17:57:01 +0000

    Added processlist_grantees: matching of grantees to processes
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@55 c8a82860-ae11-f3b5-05a3-c0c726401f9a

---
+++ common_schema/views/process/processlist_grantees.sql
@@ -0,0 +1,26 @@
+-- 
+-- Active processes sorted by current query runtime, desc (longest first). Exclude current connection.
+-- 
+CREATE OR REPLACE
+ALGORITHM = MERGE
+SQL SECURITY INVOKER
+VIEW processlist_grantees AS
+  SELECT 
+    PROCESSLIST.ID,
+    PROCESSLIST.USER,
+    PROCESSLIST.HOST,
+    USER_PRIVILEGES.GRANTEE,
+    mysql.user.user AS grantee_user,
+    mysql.user.host AS grantee_host,
+    SUM(USER_PRIVILEGES.PRIVILEGE_TYPE = 'SUPER') AS is_super,
+    (PROCESSLIST.USER = 'system user' OR PROCESSLIST.COMMAND = 'Binlog Dump') AS is_repl,
+    id = CONNECTION_ID() AS is_current,
+    CONCAT('KILL QUERY ', PROCESSLIST.ID) AS sql_kill_query,
+    CONCAT('KILL ', PROCESSLIST.ID) AS sql_kill_connection
+  FROM 
+    INFORMATION_SCHEMA.PROCESSLIST 
+    LEFT JOIN INFORMATION_SCHEMA.USER_PRIVILEGES ON (match_grantee(USER, HOST) = USER_PRIVILEGES.GRANTEE)
+    LEFT JOIN mysql.user ON (CONCAT('''', mysql.user.user, '''@''', mysql.user.host, '''') = USER_PRIVILEGES.GRANTEE)
+  GROUP BY
+    PROCESSLIST.ID, PROCESSLIST.USER, PROCESSLIST.HOST, PROCESSLIST.COMMAND, USER_PRIVILEGES.GRANTEE, mysql.user.user, mysql.user.host
+;
