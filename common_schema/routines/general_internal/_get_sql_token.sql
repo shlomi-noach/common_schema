13ed7649a2af40f41f04a3d9ee6999d827112fd8
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2011-09-15 08:57:34 +0000

    _get_general_tokem: supports block comment and others _wrap_select_list_columns: support padding of columns to a given value
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@84 c8a82860-ae11-f3b5-05a3-c0c726401f9a

--- common_schema/routines/general_internal/_get_sql_token.sql
+++ common_schema/routines/general_internal/_get_sql_token.sql
@@ -9,7 +9,7 @@ drop procedure if exists _get_sql_token;
 create procedure _get_sql_token(
     in      p_text      text charset utf8
 ,   inout   p_from      int unsigned
-,   inout   p_level     int unsigned
+,   inout   p_level     int
 ,   out     p_token     text charset utf8
 ,   inout   p_state     enum(
                             'alpha'
@@ -22,6 +22,7 @@ create procedure _get_sql_token(
                         ,   'bitwise xor'
                         ,   'colon'
                         ,   'comma'                        
+                        ,   'conditional comment'
                         ,   'decimal'
                         ,   'delimiter'
                         ,   'divide'
@@ -84,9 +85,6 @@ begin
         set v_char = substr(p_text, v_from, 1)
         ,   v_lookahead = substr(p_text, v_from+1, 1)
         ;
-        if @debug_get_sql_token then
-            select p_state, v_from, v_char;
-        end if;
         state_case: begin case p_state
             when 'error' then 
                 set p_from = v_length;
@@ -97,9 +95,14 @@ begin
                         set p_state = 'integer';
                     when v_char between 'A' and 'Z' 
                     or   v_char between 'a' and 'z' 
-                    or   v_char = '_' then 
-                        set p_state = 'alpha';
-                    when v_char in (' ', '\t', '\n', '\r') then 
+                    or   v_char = '_' then
+                        set p_state = 'alpha';                        
+                    when v_char = ' ' then 
+                        set p_state = 'whitespace'
+                        ,   v_from = v_length - character_length(ltrim(substring(p_text, v_from)))
+                        ;
+                        leave state_case;
+                    when v_char in ('\t', '\n', '\r') then 
                         set p_state = 'whitespace';
                     when v_char = '''' or v_no_ansi_quotes and v_char = '"' then
                         set p_state = 'string', v_quote_char = v_char;
@@ -135,7 +138,15 @@ begin
                         leave my_loop;
                     when v_char = '/' then
                         if v_lookahead = '*' then
-                            set p_state = 'multi line comment', v_from = v_from + 1;
+                            set v_from = locate('*/', p_text, p_from + 2);
+                            if v_from then
+                                set p_state = if (substr(p_text, p_from + 2, 1) = '!', 'conditional comment', 'multi line comment')
+                                ,   v_from = v_from + 2
+                                ;
+                                leave my_loop;
+                            else
+                                set p_state = 'error';
+                            end if;
                         else
                             set p_state = 'divide', v_from = v_from + 1;
                             leave my_loop;
@@ -213,7 +224,6 @@ begin
                         leave my_loop;
                     else 
                         set p_state = 'error';
-                        leave my_loop;
                 end case;
             when 'less than' then
                 case v_char 
@@ -227,7 +237,6 @@ begin
                         leave my_loop;
                     else
                         set p_state = 'error';
-                        leave my_loop;
                 end case;
             when 'less than or equals' then
                 if v_char = '>' then 
@@ -246,7 +255,6 @@ begin
                         leave my_loop;
                     else
                         set p_state = 'error';
-                        leave my_loop;
                 end case;
             when 'multi line comment' then
                 if v_char = '*' and v_lookahead = '/' then
@@ -291,23 +299,18 @@ begin
                         leave my_loop;
                 end case;
             when 'whitespace' then
-                case 
-                    when v_char in (' ', '\t', '\n', '\r') then
-                        leave state_case;
-                    else 
-                        leave my_loop;                        
-                end case;
+                if v_char not in ('\t', '\n', '\r') then
+                    leave my_loop;                        
+                end if;
             when 'string' then
-                if v_char != v_quote_char then 
-                    leave state_case;
+                if v_char = v_quote_char then 
+                    if v_lookahead = v_quote_char then
+                        set v_from = v_from + 2;
+                    elseif substr(p_text, v_from - 2, 1) != '\\' then 
+                        set v_from = v_from + 1;
+                        leave my_loop;
+                    end if;
                 end if;
-                set v_from = v_from + 1;
-                if substr(p_text, v_from - 2, 1) = '\\' then 
-                    leave state_case;
-                elseif v_lookahead = v_quote_char then
-                    leave state_case;
-                end if;
-                leave my_loop;
             when 'quoted identifier' then
                 if v_char != v_quote_char then 
                     leave state_case;
--- common_schema/routines/general_internal/_wrap_select_list_columns.sql
+++ common_schema/routines/general_internal/_wrap_select_list_columns.sql
@@ -5,74 +5,82 @@ drop procedure if exists _wrap_select_list_columns
 
 create procedure _wrap_select_list_columns(
     inout p_text text
+,   in p_column_count int
 ,   out p_error  text
 )
-begin
+my_proc: begin
     declare v_from, v_old_from int unsigned;
     declare v_token text;
     declare v_level int unsigned;
     declare v_state varchar(32);
-    declare v_select bool default false;
     declare v_whitespace varchar(1) default '';
     declare v_done bool default FALSE;
     declare v_statement text;
     declare v_expression text default '';
-    declare v_column_number int unsigned default 1;
+    declare v_column_number int unsigned default 0;
 
+my_main: begin
     -- part one: find the SELECT keyword.
     my_loop: repeat 
         set v_old_from = v_from;
         call _get_sql_token(p_text, v_from, v_level, v_token, v_state);
         case 
-            when v_state in ('whitespace', 'single line comment', 'multi line comment') then
+            when v_state in ('whitespace', 'single line comment', 'multi line comment', 'conditional comment') then
                 iterate my_loop;
-            when v_select = false and v_state = 'alpha' and v_token = 'select' then
-                set v_select = true;
-            else leave my_loop;
-        end case;
-    until 
-        v_select
-    or  v_old_from = v_from
-    end repeat;
-    
-    if v_select = false then
-        set p_error = 'no SELECT keyword found';
-    end if;
-
-    set v_statement = substr(p_text, 1, v_from);
-    -- part two: rewrite columns
-    my_loop: repeat 
-        set v_old_from = v_from;
-        call _get_sql_token(p_text, v_from, v_level, v_token, v_state);         
-        if v_level = 0 and v_token in ('from', ',') then
-            set v_statement = concat(
-                    v_statement
-                ,   '(select '
-                ,   v_expression
-                ,   ') as col'
-                ,   v_column_number
-                ,   ' '
-                ,   v_token
-                )
-            ;
-            if v_token = 'from' then
-                set v_statement = concat(
-                        v_statement
-                    ,   substr(p_text, v_from)
-                    );
+            when v_state = 'alpha' and v_token = 'select' then
+                set v_statement = substr(p_text, 1, v_from);
                 leave my_loop;
-            else
-                set v_column_number = v_column_number + 1
-                ,   v_expression = ''
-                ;
-            end if;
-        else
-            set v_expression = concat(v_expression, v_token);
-        end if;
+            else 
+                set p_error = 'No Select found';
+                leave my_proc;
+        end case;
     until 
         v_old_from = v_from
     end repeat;
-    set p_text = v_statement;
+    
+    -- part two: rewrite columns
+    my_loop: repeat 
+        set v_old_from = v_from;
+        call _get_sql_token(p_text, v_from, v_level, v_token, v_state);
+        if v_column_number < p_column_count then
+            if  v_level = 0 and (
+                (v_state, v_token) in (('alpha', v_from), ('comma', ',')) 
+            or  v_old_from = v_from
+            )
+            then
+                set v_statement = concat(
+                        v_statement
+                    ,   if (v_column_number, ', ', '')
+                    ,   '(select '
+                    ,   v_expression
+                    ,   ') as col'
+                    ,   (v_column_number + 1)
+                    )
+                ,   v_column_number = v_column_number + 1
+                ,   v_expression = ''
+                ;
+            else
+                set v_expression = concat(v_expression, v_token);
+            end if;
+        end if;
+    until 
+        v_old_from = v_from or v_token = 'from'
+    end repeat;
+
+    -- part three: pad null columns
+    while v_column_number < p_column_count do
+        set v_column_number = v_column_number + 1
+        ,   v_statement = concat(v_statement, ', null as col', v_column_number)
+        ;
+    end while;
+
+    end;
+    
+    set p_text= concat(
+                    v_statement
+                ,   if(v_token = 'from', ' from', '')
+                ,   substr(p_text, v_from)
+                );
 end;
 //
 
