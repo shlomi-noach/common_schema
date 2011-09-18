1213ae918f22156720fe8b79f83fb81472d37b51
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2011-09-18 06:14:41 +0000

    added _is_select_query(): heuristic test for SELECT query input
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@102 c8a82860-ae11-f3b5-05a3-c0c726401f9a

---
+++ common_schema/routines/general/_is_select_query.sql
@@ -0,0 +1,25 @@
+-- 
+-- Returns 1 when given input starts with SELECT, 0 otherwise
+-- 
+-- This is heuristic only, and is used to diagnose input to various routines.
+-- 
+-- Example:
+--
+-- SELECT _is_select_query('SELECT 3 FROM DUAL');
+-- Returns: 1
+--
+
+DELIMITER $$
+
+DROP FUNCTION IF EXISTS _is_select_query $$
+CREATE FUNCTION _is_select_query(input LONGTEXT CHARSET utf8) RETURNS TINYINT UNSIGNED
+DETERMINISTIC
+NO SQL
+SQL SECURITY INVOKER
+COMMENT 'Returns 1 when given input starts with SELECT'
+
+BEGIN
+  RETURN (LOCATE('select', LOWER(LTRIM(input))) = 1);
+END $$
+
+DELIMITER ;
