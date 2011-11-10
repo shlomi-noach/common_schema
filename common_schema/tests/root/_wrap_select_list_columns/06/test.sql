c29864f89893fd42e6656373a8ea7dd8fff79e0c
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2011-11-10 11:01:32 +0000

    _wrap_select_list_columns modified due to changes in revision 149
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@150 c8a82860-ae11-f3b5-05a3-c0c726401f9a

--- common_schema/tests/root/_wrap_select_list_columns/06/test.sql
+++ common_schema/tests/root/_wrap_select_list_columns/06/test.sql
@@ -1,3 +1,3 @@
-SET @query := 'SELECT rank, COUNT(*) AS num_winners, GROUP_CONCAT(name) AS names FROM test_cs.test__wrap_select_list_columns GROUP BY rank ORDER BY rank ASC';
+SET @query := 'SELECT rank, COUNT(*) AS num_winners, GROUP_CONCAT(name ORDER BY name) AS names FROM test_cs.test__wrap_select_list_columns GROUP BY rank ORDER BY rank ASC';
 CALL _wrap_select_list_columns(@query, 3, @error);
 CALL exec(@query);
