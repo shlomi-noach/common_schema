7205185997ad7a93f9ca52ba05662d071605916d
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2011-11-23 09:32:17 +0000

    Added _retokenized_queries() function, which splits queries, then concatenates again with special internal unlikely token Added tests for _retokenized_queries()
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@169 c8a82860-ae11-f3b5-05a3-c0c726401f9a

---
+++ common_schema/tests/root/_retokenized_queries/02/test.sql
@@ -0,0 +1,5 @@
+SET @queries := 'select `1` from dual; select ''2'', "3", ''a"b'', "it''s" from dual;';
+SELECT 
+  _retokenized_queries(@queries) = 
+    'select `1` from dual[\0_cs_::;::\0] select ''2'', "3", ''a"b'', "it''s" from dual[\0_cs_::;::\0]';
+;
