db77d17d9a2c4525a3caaad5594ae02acccc016d
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2011-12-20 13:10:48 +0000

    Added help_contents table
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@190 c8a82860-ae11-f3b5-05a3-c0c726401f9a

---
+++ common_schema/tables/help_content.sql
@@ -0,0 +1,11 @@
+-- 
+-- Metadata: information about this project
+-- 
+DROP TABLE IF EXISTS help_content;
+
+CREATE TABLE help_content (
+  topic VARCHAR(32) CHARSET ascii NOT NULL,
+  help_message TEXT CHARSET utf8 NOT NULL,
+  PRIMARY KEY (topic)
+)
+;
