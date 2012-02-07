21978fb73540b0cb55e2d7b6e83cac4af16cdff4
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2012-02-07 11:30:03 +0000

    Updated copyright notice
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@217 c8a82860-ae11-f3b5-05a3-c0c726401f9a

--- common_schema/main_head.sql
+++ common_schema/main_head.sql
@@ -3,7 +3,7 @@
 --
 -- Released under the BSD license
 --
--- Copyright (c) 2011, Shlomi Noach
+-- Copyright (c) 2011 - 2012, Shlomi Noach
 -- All rights reserved.
 --
 -- Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
--- common_schema/tables/metadata.sql
+++ common_schema/tables/metadata.sql
@@ -20,7 +20,7 @@ VALUES
   ('author_url', 'http://code.openark.org/blog/shlomi-noach'),
   ('license_type', 'New BSD'),
   ('license', '
-Copyright (c) 2011, Shlomi Noach
+Copyright (c) 2011 - 2012, Shlomi Noach
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
