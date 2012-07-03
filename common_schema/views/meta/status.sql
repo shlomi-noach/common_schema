7174561256b5f67dc775afbf0a17ca6e3fb67479
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2012-07-03 18:02:31 +0000

    status.sql: - added columns indicating install status for the various components of common_schema - added info on MySQL server version
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@237 c8a82860-ae11-f3b5-05a3-c0c726401f9a

--- common_schema/views/meta/status.sql
+++ common_schema/views/meta/status.sql
@@ -11,8 +11,11 @@ VIEW status AS
     max(if(attribute_name = 'revision', attribute_value, null)) as revision,
     max(if(attribute_name = 'install_time', attribute_value, null)) as install_time,
     max(if(attribute_name = 'install_success', attribute_value, null)) as install_success,    
+    max(if(attribute_name = 'base_components_installed', attribute_value, null)) as base_components_installed,    
+    max(if(attribute_name = 'innodb_plugin_components_installed', attribute_value, null)) as innodb_plugin_components_installed,    
+    max(if(attribute_name = 'percona_server_components_installed', attribute_value, null)) as percona_server_components_installed,    
+    max(if(attribute_name = 'install_mysql_version', attribute_value, null)) as install_mysql_version,
     max(if(attribute_name = 'install_sql_mode', attribute_value, null)) as install_sql_mode
   from
     metadata;
 ;
-
