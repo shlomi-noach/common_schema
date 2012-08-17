3ec4c2f3f7adcbd66a2cb3ba41e37ecb88dbcaa7
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2012-08-17 14:21:44 +0000

    Revised build_data
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@288 c8a82860-ae11-f3b5-05a3-c0c726401f9a

--- common_schema/build_data/innodb_plugin_component.sql
+++ common_schema/build_data/innodb_plugin_component.sql
@@ -3,10 +3,11 @@
 --
 set @script := "
 try {
+  set @common_schema_innodb_plugin_expected := @common_schema_innodb_plugin_expected + 1; 
 component.placeholder
+  set @common_schema_innodb_plugin_installed := @common_schema_innodb_plugin_installed + 1;
 }
 catch {
-  set @common_schema_innodb_plugin_installed := false;
 }
 ";
 
--- common_schema/build_data/percona_server_component.sql
+++ common_schema/build_data/percona_server_component.sql
@@ -3,10 +3,11 @@
 --
 set @script := "
 try {
+  set @common_schema_percona_server_expected := @common_schema_percona_server_expected + 1; 
 component.placeholder
+  set @common_schema_percona_server_installed := @common_schema_percona_server_installed + 1;
 }
 catch {
-  set @common_schema_percona_server_installed := false;
 }
 ";
 
