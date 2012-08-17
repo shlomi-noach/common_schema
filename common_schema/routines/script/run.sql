77e06cbc98cce64896880f78caf48bfaae3e255a
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2012-08-17 14:06:35 +0000

    run(): auto detect file name, load script from file on such event
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@270 c8a82860-ae11-f3b5-05a3-c0c726401f9a

--- common_schema/routines/script/run.sql
+++ common_schema/routines/script/run.sql
@@ -16,6 +16,17 @@ modifies sql data
 sql security invoker
 
 main_body: begin
+  if (LEFT(query_script, 1) in ('/', '\\')) and (LEFT(query_script, 2) != '/*') then
+    begin
+	  declare query_script_file_name text;  
+      -- Assume filename
+      set query_script_file_name := query_script;
+      set query_script := LOAD_FILE(query_script_file_name);
+      if query_script is null then
+        call throw(CONCAT('Cannot load script file: ', query_script_file_name));
+      end if;
+    end;
+  end if;
   call _interpret(query_script, TRUE);
 end;
 
