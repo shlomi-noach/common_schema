b5db27b1f55804610968d04d0e6ef8f12f475fe3
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2012-09-27 12:47:57 +0000

    Added slave_status view - Added documentation for the above
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@334 c8a82860-ae11-f3b5-05a3-c0c726401f9a

---
+++ common_schema/views/process/slave_status.sql
@@ -0,0 +1,16 @@
+-- 
+-- Provide with slave status info
+-- 
+CREATE OR REPLACE
+ALGORITHM = TEMPTABLE
+SQL SECURITY INVOKER
+VIEW slave_status AS
+  SELECT 
+    SUM(IF(is_io_thread, TIME, NULL)) AS Slave_Connected_time,
+    SUM(is_io_thread) IS TRUE AS Slave_IO_Running,
+    SUM(is_sql_thread) IS TRUE AS Slave_SQL_Running,
+    (SUM(is_io_thread) AND SUM(is_sql_thread)) IS TRUE AS Slave_Running,
+    SUM(IF(is_sql_thread, TIME, NULL)) AS Seconds_Behind_Master
+  FROM 
+    processlist_repl
+;
