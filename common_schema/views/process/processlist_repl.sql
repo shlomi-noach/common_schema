f79b6bff1d089f47e4a7c96a9aeb0d4c29af2e35
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2012-09-27 12:35:58 +0000

    processlist_repl now includes is_io_thread, is_sql_thread columns, via _known_thread_states - updated documentation
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@333 c8a82860-ae11-f3b5-05a3-c0c726401f9a

--- common_schema/views/process/processlist_repl.sql
+++ common_schema/views/process/processlist_repl.sql
@@ -6,11 +6,14 @@ ALGORITHM = MERGE
 SQL SECURITY INVOKER
 VIEW processlist_repl AS
   SELECT 
-    *,
+    PROCESSLIST.*,
     USER = 'system user' AS is_system,
+    (USER = 'system user' AND state_type = 'replication_io_thread') IS TRUE AS is_io_thread,
+    (USER = 'system user' AND state_type = 'replication_sql_thread') IS TRUE AS is_sql_thread,
     COMMAND = 'Binlog Dump' AS is_slave
   FROM 
     INFORMATION_SCHEMA.PROCESSLIST 
+    LEFT JOIN _known_thread_states ON (_known_thread_states.state LIKE CONCAT(PROCESSLIST.STATE, '%'))
   WHERE 
     USER = 'system user'
     OR COMMAND = 'Binlog Dump'
