c3b85b34d50aae1c5d94998cefcfdcc28dc3caf5
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2012-09-28 05:33:04 +0000

    processlist_per_userhost with new more process run time info columns + documentation
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@338 c8a82860-ae11-f3b5-05a3-c0c726401f9a

--- common_schema/views/process/processlist_per_userhost.sql
+++ common_schema/views/process/processlist_per_userhost.sql
@@ -10,6 +10,9 @@ VIEW processlist_per_userhost AS
     MIN(SUBSTRING_INDEX(HOST, ':', 1)) AS host,
     COUNT(*) AS count_processes,
     SUM(COMMAND != 'Sleep') AS active_processes,
+    CAST(split_token(GROUP_CONCAT(IF(COMMAND != 'Sleep', TIME, NULL) ORDER BY TIME), ',', COUNT(IF(COMMAND != 'Sleep', TIME, NULL))/2) AS DECIMAL(10,2)) AS median_active_time,
+    CAST(split_token(GROUP_CONCAT(IF(COMMAND != 'Sleep', TIME, NULL) ORDER BY TIME), ',', COUNT(IF(COMMAND != 'Sleep', TIME, NULL))*95/100) AS DECIMAL(10,2)) AS median_95pct_active_time,
+    MAX(IF(COMMAND != 'Sleep', TIME, NULL)) AS max_active_time,
     AVG(IF(COMMAND != 'Sleep', TIME, NULL)) AS average_active_time
   FROM 
     INFORMATION_SCHEMA.PROCESSLIST 
