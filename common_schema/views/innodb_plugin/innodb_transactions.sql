8c4b60d6c042d44786ba2e6b6f1d73b98f582a17
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2012-09-28 05:21:03 +0000

    innodb_transactions with: - KILL, KILL QUERY columns - trx runtime, lock time and wait time columns + documentation
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@337 c8a82860-ae11-f3b5-05a3-c0c726401f9a

--- common_schema/views/innodb_plugin/innodb_transactions.sql
+++ common_schema/views/innodb_plugin/innodb_transactions.sql
@@ -6,10 +6,16 @@ ALGORITHM = MERGE
 SQL SECURITY INVOKER
 VIEW innodb_transactions AS
   SELECT 
-    * 
+    INFORMATION_SCHEMA.INNODB_TRX.*, 
+    PROCESSLIST.INFO,
+    TIMESTAMPDIFF(SECOND, trx_started, SYSDATE()) as trx_runtime_seconds,
+    TIMESTAMPDIFF(SECOND, trx_wait_started, SYSDATE()) as trx_wait_seconds,
+    IF(PROCESSLIST.COMMAND = 'Sleep', PROCESSLIST.TIME, 0) AS trx_idle_seconds,
+    CONCAT('KILL QUERY ', trx_mysql_thread_id) AS sql_kill_query,
+    CONCAT('KILL ', trx_mysql_thread_id) AS sql_kill_connection    
   FROM 
     INFORMATION_SCHEMA.INNODB_TRX
+    LEFT JOIN INFORMATION_SCHEMA.PROCESSLIST ON (trx_mysql_thread_id = PROCESSLIST.ID)
   WHERE 
-    trx_query IS NOT NULL
-    AND trx_mysql_thread_id != CONNECTION_ID()
+    trx_mysql_thread_id != CONNECTION_ID()
 ;
