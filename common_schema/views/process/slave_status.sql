c494e160d9d8b2bb6470cb151d054c847c13c010
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2012-09-29 19:39:39 +0000

    slave_status: better identification of the sql_thread (also work by elimination) resulting with better Seconds_Behind_master: no funny NULLs
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@349 c8a82860-ae11-f3b5-05a3-c0c726401f9a

--- common_schema/views/process/slave_status.sql
+++ common_schema/views/process/slave_status.sql
@@ -8,9 +8,9 @@ VIEW slave_status AS
   SELECT 
     SUM(IF(is_io_thread, TIME, NULL)) AS Slave_Connected_time,
     SUM(is_io_thread) IS TRUE AS Slave_IO_Running,
-    SUM(is_sql_thread) IS TRUE AS Slave_SQL_Running,
-    (SUM(is_io_thread) AND SUM(is_sql_thread)) IS TRUE AS Slave_Running,
-    SUM(IF(is_sql_thread, TIME, NULL)) AS Seconds_Behind_Master
+    SUM(is_sql_thread OR (is_system AND NOT is_io_thread)) IS TRUE AS Slave_SQL_Running,
+    (SUM(is_system) = 2) IS TRUE AS Slave_Running,
+    SUM(IF(is_sql_thread OR (is_system AND NOT is_io_thread), TIME, NULL)) AS Seconds_Behind_Master
   FROM 
     processlist_repl
 ;
