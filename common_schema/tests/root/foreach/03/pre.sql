acb20672f3884429532416d1fc9a3121ce1c0736
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2012-10-05 13:18:50 +0000

    Fixed foreach() double number test
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@352 c8a82860-ae11-f3b5-05a3-c0c726401f9a

--- common_schema/tests/root/foreach/03/pre.sql
+++
@@ -1,6 +0,0 @@
-USE test_cs;
-DROP TABLE IF EXISTS test_foreach;
-CREATE TABLE test_foreach (id INT UNSIGNED, name VARCHAR(10) CHARSET ascii);
-INSERT INTO test_foreach VALUES (1, 'first');
-INSERT INTO test_foreach VALUES (2, 'second');
-INSERT INTO test_foreach VALUES (3, 'third');
