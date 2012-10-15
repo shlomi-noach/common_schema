513d31b344a702ce30b17c3f950d2b111f62c9e7
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2012-10-15 09:41:56 +0000

    fixed issue 28 _sql_tokens table now does not use auto_increment. Instead, we manually increment and INSERT the id.
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@359 c8a82860-ae11-f3b5-05a3-c0c726401f9a

--- common_schema/routines/script/_get_sql_tokens.sql
+++ common_schema/routines/script/_get_sql_tokens.sql
@@ -19,10 +19,11 @@ begin
     declare v_token text;
     declare v_level int;
     declare v_state varchar(32);
+    declare _sql_tokens_id int unsigned default 0;
     
     drop temporary table if exists _sql_tokens;
     create temporary table _sql_tokens(
-        id int unsigned auto_increment primary key
+        id int unsigned primary key
     ,   start int unsigned  not null
     ,   level int not null
     ,   token text          
@@ -32,8 +33,9 @@ begin
     repeat 
         set v_old_from = v_from;
         call _get_sql_token(p_text, v_from, v_level, v_token, 1, v_state);
-        insert into _sql_tokens(start,level,token,state) 
-        values (v_from, v_level, v_token, v_state);
+        set _sql_tokens_id := _sql_tokens_id + 1;
+        insert into _sql_tokens(id,start,level,token,state) 
+        values (_sql_tokens_id, v_from, v_level, v_token, v_state);
     until 
         v_old_from = v_from
     end repeat;
