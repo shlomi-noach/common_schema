e1dd41fcd22a81d54de2b52d754a7c14f06e12ed
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2012-11-16 07:24:52 +0000

    unquote: now checking for quotes and escaped quotes within text - added tests
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@369 c8a82860-ae11-f3b5-05a3-c0c726401f9a

--- common_schema/routines/text/unquote.sql
+++ common_schema/routines/text/unquote.sql
@@ -20,21 +20,46 @@ SQL SECURITY INVOKER
 COMMENT 'Unquotes a given text'
 
 begin
+  declare quoting_char VARCHAR(1) CHARSET utf8;
+  declare terminating_quote_escape_char VARCHAR(1) CHARSET utf8;
+  declare current_pos INT UNSIGNED;
+  declare end_quote_pos INT UNSIGNED;
+
   if CHAR_LENGTH(txt) < 2 then
     return txt;
   end if;
-  if LEFT(txt, 1) = '/' AND RIGHT(txt, 1) = '/' then
-    return SUBSTRING(txt, 2, CHAR_LENGTH(txt) - 2);
+  
+  set quoting_char := LEFT(txt, 1);
+  if not quoting_char in ('''', '"', '`', '/') then
+    return txt;
   end if;
-  if LEFT(txt, 1) = '\'' AND RIGHT(txt, 1) = '\'' then
-    return SUBSTRING(txt, 2, CHAR_LENGTH(txt) - 2);
+  if txt in ('''''', '""', '``', '//') then
+    return '';
   end if;
-  if LEFT(txt, 1) = '\"' AND RIGHT(txt, 1) = '\"' then
-    return SUBSTRING(txt, 2, CHAR_LENGTH(txt) - 2);
-  end if;
-  if LEFT(txt, 1) = '`' AND RIGHT(txt, 1) = '`' then
-    return SUBSTRING(txt, 2, CHAR_LENGTH(txt) - 2);
-  end if;
+  
+  set current_pos := 1;
+  terminating_quote_loop: while current_pos > 0 do
+    set current_pos := LOCATE(quoting_char, txt, current_pos + 1);
+    if current_pos = 0 then
+      -- No terminating quote
+      return txt;
+    end if;
+    if SUBSTRING(txt, current_pos, 2) = REPEAT(quoting_char, 2) then
+      set current_pos := current_pos + 1;
+      iterate terminating_quote_loop;
+    end if;
+    set terminating_quote_escape_char := SUBSTRING(txt, current_pos - 1, 1);
+    if (terminating_quote_escape_char = quoting_char) or (terminating_quote_escape_char = '\\') then
+      -- This isn't really a quote end: the quote is escaped. 
+      -- We do nothing; just a trivial assignment.
+      iterate terminating_quote_loop;
+    end if;
+    -- Found terminating quote.
+    leave terminating_quote_loop;
+  end while;
+  if current_pos = CHAR_LENGTH(txt) then
+      return SUBSTRING(txt, 2, CHAR_LENGTH(txt) - 2);
+    end if;
   return txt;
 end $$
 
