6db3ea0fc312982bfc47bab58fb1b08905530b55
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2013-02-03 08:27:55 +0000

    _concatenate_array_elements(): renamed a parameter from `delimiter` (a reserved keyword)
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@433 c8a82860-ae11-f3b5-05a3-c0c726401f9a

--- common_schema/routines/script/array/tarray/_concatenate_array_elements.sql
+++ common_schema/routines/script/array/tarray/_concatenate_array_elements.sql
@@ -7,7 +7,7 @@ drop procedure if exists _concatenate_array_elements //
 
 create procedure _concatenate_array_elements(
    in  array_id VARCHAR(16) charset ascii,
-   in   delimiter text charset utf8,
+   in   delimiter_text text charset utf8,
    out   result text charset utf8
 ) 
 comment 'Creates a populated array, returning its ID'
@@ -21,7 +21,7 @@ main_body: begin
   
   set @_common_schema_get_array_element_value := NULL;
   set exec_statement := CONCAT(
-    'SELECT GROUP_CONCAT(value ORDER BY array_key SEPARATOR ''', delimiter ,''') FROM ', _get_array_name(array_id), 
+    'SELECT GROUP_CONCAT(value ORDER BY array_key SEPARATOR ''', delimiter_text ,''') FROM ', _get_array_name(array_id), 
        ' INTO @_common_schema_concatenate_array_elements_value');
 
   call exec_single(exec_statement);
