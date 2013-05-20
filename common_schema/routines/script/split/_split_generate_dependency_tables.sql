b4619396e32fdbbe537f7d76e4c64291a1690cad
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2013-05-20 07:13:56 +0000

    split: better algorithm for choosing candidate key
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@457 c8a82860-ae11-f3b5-05a3-c0c726401f9a

--- common_schema/routines/script/split/_split_generate_dependency_tables.sql
+++ common_schema/routines/script/split/_split_generate_dependency_tables.sql
@@ -8,29 +8,27 @@ DROP PROCEDURE IF EXISTS _split_generate_dependency_tables $$
 CREATE PROCEDURE _split_generate_dependency_tables(split_table_schema varchar(128), split_table_name varchar(128)) 
 MODIFIES SQL DATA
 SQL SECURITY INVOKER
-COMMENT 'split values by columns...'
+COMMENT 'analyze recommended keys'
 
 begin
   declare split_column_names varchar(2048) default NULL;
   declare split_num_column tinyint unsigned;
   
-  drop temporary table if exists _split_unique_keys;
-  create temporary table _split_unique_keys
+  drop temporary table if exists _split_unique_key_columns;
+  create temporary table _split_unique_key_columns
    SELECT
       TABLE_SCHEMA,
       TABLE_NAME,
       INDEX_NAME,
-      COUNT(*) AS COUNT_COLUMN_IN_INDEX,
-      IF(SUM(NULLABLE = 'YES') > 0, 1, 0) AS has_nullable,
-      IF(INDEX_NAME = 'PRIMARY', 1, 0) AS is_primary,
-      GROUP_CONCAT(COLUMN_NAME ORDER BY SEQ_IN_INDEX ASC) AS COLUMN_NAMES,
-      SUBSTRING_INDEX(GROUP_CONCAT(COLUMN_NAME ORDER BY SEQ_IN_INDEX ASC), ',', 1) AS FIRST_COLUMN_NAME
+      COLUMN_NAME,
+      SEQ_IN_INDEX,
+      (NULLABLE = 'YES') AS has_nullable,
+      (INDEX_NAME = 'PRIMARY') AS is_primary
     FROM INFORMATION_SCHEMA.STATISTICS
     WHERE
       TABLE_SCHEMA = split_table_schema
       AND TABLE_NAME = split_table_name
       AND NON_UNIQUE=0
-    GROUP BY TABLE_SCHEMA, TABLE_NAME, INDEX_NAME
   ;
   
   drop temporary table if exists _split_i_s_columns;
@@ -39,8 +37,37 @@ begin
       TABLE_SCHEMA,
       TABLE_NAME,
       COLUMN_NAME,
-      DATA_TYPE,
-      CHARACTER_SET_NAME
+      CASE LOWER(DATA_TYPE)
+          WHEN 'tinyint' THEN 1
+          WHEN 'smallint' THEN 2
+          WHEN 'mediumint' THEN 4
+          WHEN 'int' THEN 4
+          WHEN 'bigint' THEN 8
+          WHEN 'timestamp' THEN 4
+          WHEN 'time' THEN 4
+          WHEN 'date' THEN 4
+          WHEN 'datetime' THEN 8
+          WHEN 'float' THEN 4
+          WHEN 'double' THEN 8
+          WHEN 'decimal' THEN CEILING(NUMERIC_PRECISION/2)
+          WHEN 'bit' THEN CEILING(NUMERIC_PRECISION/8)
+          WHEN 'set' THEN 64
+          WHEN 'enum' THEN 8
+          WHEN 'year' THEN 2
+          WHEN 'char' THEN CHARACTER_OCTET_LENGTH
+          WHEN 'varchar' THEN CHARACTER_OCTET_LENGTH
+          WHEN 'binary' THEN CHARACTER_OCTET_LENGTH
+          WHEN 'varbinary' THEN CHARACTER_OCTET_LENGTH
+          WHEN 'tinytext' THEN CHARACTER_OCTET_LENGTH
+          WHEN 'text' THEN CHARACTER_OCTET_LENGTH
+          WHEN 'mediumtext' THEN CHARACTER_OCTET_LENGTH
+          WHEN 'longtext' THEN CHARACTER_OCTET_LENGTH
+          WHEN 'tinyblob' THEN CHARACTER_OCTET_LENGTH
+          WHEN 'blob' THEN CHARACTER_OCTET_LENGTH
+          WHEN 'mediumblob' THEN CHARACTER_OCTET_LENGTH
+          WHEN 'longblob' THEN CHARACTER_OCTET_LENGTH
+          ELSE 255
+        END AS column_type_length
     FROM INFORMATION_SCHEMA.COLUMNS
     WHERE
       TABLE_SCHEMA = split_table_schema
@@ -49,62 +76,31 @@ begin
   
   drop temporary table if exists _split_candidate_keys;
   create temporary table _split_candidate_keys
-    SELECT
-      _split_i_s_columns.TABLE_SCHEMA AS table_schema,
-      _split_i_s_columns.TABLE_NAME AS table_name,
-      _split_unique_keys.INDEX_NAME AS index_name,
-      _split_unique_keys.has_nullable AS has_nullable,
-      _split_unique_keys.is_primary AS is_primary,
-      _split_unique_keys.COLUMN_NAMES AS column_names,
-      _split_unique_keys.COUNT_COLUMN_IN_INDEX AS count_column_in_index,
-      _split_i_s_columns.DATA_TYPE AS data_type,
-      _split_i_s_columns.CHARACTER_SET_NAME AS character_set_name,
-      (if (has_nullable, 1, 0) << 21) 
-        +
-        (CASE IFNULL(CHARACTER_SET_NAME, '')
-            WHEN '' THEN 0
-            ELSE 1
-        END << 20
-        )
-        + (CASE LOWER(DATA_TYPE)
-          WHEN 'tinyint' THEN 0
-          WHEN 'smallint' THEN 1
-          WHEN 'int' THEN 2
-          WHEN 'timestamp' THEN 3
-          WHEN 'bigint' THEN 4
-          WHEN 'datetime' THEN 5
-          ELSE 9
-        END << 16
-        ) + (COUNT_COLUMN_IN_INDEX << 0
-      ) AS candidate_key_rank_in_table  
+   SELECT
+      TABLE_SCHEMA,
+      TABLE_NAME,
+      INDEX_NAME,
+      MAX(has_nullable) AS has_nullable,
+      MAX(is_primary) AS is_primary,
+      COUNT(*) AS count_column_in_index,
+      GROUP_CONCAT(COLUMN_NAME ORDER BY SEQ_IN_INDEX ASC) AS COLUMN_NAMES,
+      SUM(column_type_length) AS covered_columns_length
     FROM 
-      _split_i_s_columns
-      INNER JOIN _split_unique_keys ON (
-        _split_i_s_columns.TABLE_SCHEMA = _split_unique_keys.TABLE_SCHEMA AND
-        _split_i_s_columns.TABLE_NAME = _split_unique_keys.TABLE_NAME AND
-        _split_i_s_columns.COLUMN_NAME = _split_unique_keys.FIRST_COLUMN_NAME
-      )
-    ORDER BY   
-      _split_i_s_columns.TABLE_SCHEMA, _split_i_s_columns.TABLE_NAME, candidate_key_rank_in_table
+      _split_unique_key_columns
+      JOIN _split_i_s_columns USING(TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME)
+    GROUP BY TABLE_SCHEMA, TABLE_NAME, INDEX_NAME
   ;
-  
+
   drop temporary table if exists _split_candidate_keys_recommended;
   create temporary table _split_candidate_keys_recommended
     SELECT
-      table_schema,
-      table_name,	
-      SUBSTRING_INDEX(GROUP_CONCAT(index_name ORDER BY candidate_key_rank_in_table ASC), ',', 1) AS recommended_index_name,
-      CAST(SUBSTRING_INDEX(GROUP_CONCAT(has_nullable ORDER BY candidate_key_rank_in_table ASC), ',', 1) AS UNSIGNED INTEGER) AS has_nullable,
-      CAST(SUBSTRING_INDEX(GROUP_CONCAT(is_primary ORDER BY candidate_key_rank_in_table ASC), ',', 1) AS UNSIGNED INTEGER) AS is_primary,
-      CAST(SUBSTRING_INDEX(GROUP_CONCAT(count_column_in_index ORDER BY candidate_key_rank_in_table ASC), ',', 1) AS UNSIGNED INTEGER) AS count_column_in_index,
-      SUBSTRING_INDEX(GROUP_CONCAT(column_names ORDER BY candidate_key_rank_in_table ASC SEPARATOR '\n'), '\n', 1) AS column_names
+      *
     FROM 
       _split_candidate_keys
-    GROUP BY
-      table_schema, table_name
     ORDER BY   
-      table_schema, table_name
-    ;
+      has_nullable ASC, covered_columns_length ASC, is_primary DESC
+    LIMIT 1
+  ;
   
 end $$
 
