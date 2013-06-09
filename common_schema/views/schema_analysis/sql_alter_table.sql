79ba8893e86e506e3c861d1ec11d366edd90775b
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2013-06-09 11:49:26 +0000

    sql_alter_table: - provides ADD keys statement clause for all keys - provides DROP KEYs statement clause for all keys - Provides create-options clause outside statement
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@471 c8a82860-ae11-f3b5-05a3-c0c726401f9a

--- common_schema/views/schema_analysis/sql_alter_table.sql
+++ common_schema/views/schema_analysis/sql_alter_table.sql
@@ -1,6 +1,59 @@
 -- 
--- Generate ALTER TABLE statements per table, with engine and create options
+-- Generate ALTER TABLE statements per table, with keys, engine and create options
 -- 
+
+
+CREATE OR REPLACE
+ALGORITHM = TEMPTABLE
+SQL SECURITY INVOKER
+VIEW _sql_table_keys AS
+  SELECT 
+    TABLE_SCHEMA,
+    TABLE_NAME,
+    INDEX_NAME,
+    CONCAT(
+      IF(MIN(NON_UNIQUE) = 0, IF(INDEX_NAME='PRIMARY', 'PRIMARY ', 'UNIQUE '), ''),
+      'KEY ', 
+      IF(INDEX_NAME='PRIMARY', '', mysql_qualify(INDEX_NAME)), '(',
+        GROUP_CONCAT(
+          mysql_qualify(COLUMN_NAME),
+          IFNULL(
+            CONCAT('(', SUB_PART,')'),
+            ''
+            )
+          ORDER BY SEQ_IN_INDEX
+        ),
+      ')'
+      ) as sql_index_definition
+  FROM 
+    INFORMATION_SCHEMA.STATISTICS
+  WHERE
+    TABLE_SCHEMA NOT IN ('mysql', 'INFORMATION_SCHEMA', 'performance_schema')
+  GROUP BY
+    TABLE_SCHEMA, TABLE_NAME, INDEX_NAME
+;
+
+
+CREATE OR REPLACE
+ALGORITHM = TEMPTABLE
+SQL SECURITY INVOKER
+VIEW sql_table_keys AS
+  SELECT 
+    TABLE_SCHEMA,
+    TABLE_NAME,
+    INDEX_NAME,
+    IF(
+      INDEX_NAME='PRIMARY',
+      'DROP PRIMARY KEY',
+      CONCAT('DROP KEY ', mysql_qualify(INDEX_NAME))
+    ) as sql_drop_key,
+    CONCAT('ADD ', sql_index_definition) as sql_add_key
+  FROM 
+    _sql_table_keys
+;
+
+
+
 CREATE OR REPLACE
 ALGORITHM = MERGE
 SQL SECURITY INVOKER
@@ -9,15 +62,24 @@ VIEW sql_alter_table AS
     TABLE_SCHEMA,
     TABLE_NAME,
     ENGINE,
+    GROUP_CONCAT(sql_drop_key SEPARATOR ', ' ORDER BY INDEX_NAME) AS sql_drop_keys,
+    GROUP_CONCAT(sql_add_key  SEPARATOR ', ' ORDER BY INDEX_NAME) AS sql_add_keys,
     CONCAT(
-      'ALTER TABLE `', TABLE_SCHEMA, '`.`', TABLE_NAME, 
-      '` ENGINE=', ENGINE, ' ',
+      'ENGINE=', ENGINE, ' ',
+      IF(CREATE_OPTIONS='partitioned', '', CREATE_OPTIONS)
+    ) AS table_options,
+    CONCAT(
+      'ALTER TABLE ', mysql_qualify(TABLE_SCHEMA), '.', mysql_qualify(TABLE_NAME), 
+      ' ENGINE=', ENGINE, ' ',
       IF(CREATE_OPTIONS='partitioned', '', CREATE_OPTIONS)
     ) AS alter_statement
   FROM 
     INFORMATION_SCHEMA.TABLES
+    JOIN sql_table_keys USING (TABLE_SCHEMA, TABLE_NAME)
   WHERE
     TABLE_SCHEMA NOT IN ('mysql', 'INFORMATION_SCHEMA', 'performance_schema')
     AND ENGINE IS NOT NULL
+  GROUP BY
+    TABLE_SCHEMA, TABLE_NAME, ENGINE, CREATE_OPTIONS
 ;
 
