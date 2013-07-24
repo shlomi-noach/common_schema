55f8dda7077103df3b54378116e8c53da1991a93
roland.bouman@gmail.com <roland.bouman@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2013-07-24 09:33:59 +0000

    Fixed the parsing 'string' tokens. There were a couple of problems causing strings like '\\' and '\'' to be parsed incorrectly.
    First, in case a quote char was found, there was first a check to see if it was followed by the same quote char, and if it did it was assumed that that formed an escape sequence. This is wrong, because there was no check to see if the first quote might have been a literal quote due to a preceding slash.
    
    If no subsequent quote char would be found, the code would check if the quote was preceded by a slash, assuming that quote would be a literal quote escaped by that slash. However, this assumption could be wrong, since that slash might itself be escaped by a preceding slash, making it a literal slash.
    
    I think I fixed it though:
    - when we see a quote, use locate to find the next quote. 
    - if we don't find it, we obviously have an unterminated string, so we bail out in the error state.
    - if we do find a quote, we must first check if that quote was escaped by a preceding backslash. This must be done with a loop: if we find a sequence of an odd number of backslashes immediately before the quote, then the quote is a literal quote that was escaped by its preceding backslash and we should continue to scan for the real closing quote. 
    - if the quote was not escaped by a preceding backslash, it can still be immediately followed by a quote char. If that is the case, we need to continue scanning for the real closing quote.
    - if we find that we must continue to scan beyond the found quote, we must check if we're still in range, because if the string happens to end immediately after the escaped quote, the string is not terminated and we must deliver the error token.
    
    
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@497 c8a82860-ae11-f3b5-05a3-c0c726401f9a

--- common_schema/routines/general_internal/_get_sql_token.sql
+++ common_schema/routines/general_internal/_get_sql_token.sql
@@ -23,7 +23,7 @@ create procedure _get_sql_token(
                         ,   'bitwise not'
                         ,   'bitwise xor'
                         ,   'colon'
-                        ,   'comma'                        
+                        ,   'comma'
                         ,   'conditional comment'
                         ,   'decimal'
                         ,   'delimiter'
@@ -43,7 +43,6 @@ create procedure _get_sql_token(
                         ,   'modulo'
                         ,   'multi line comment'
                         ,   'multiply'
-                        ,   'negate'
                         ,   'not equals'
                         ,   'null safe equals'
                         ,   'or'
@@ -59,23 +58,23 @@ create procedure _get_sql_token(
                         ,   'system variable'
                         ,   'user-defined variable'
                         ,   'query_script variable'
-                        ,	'expanded query_script variable'
+                        ,   'expanded query_script variable'
                         ,   'whitespace'
                         ,   'not'
-                        )               
+                        )
 )
 comment 'Reads a token according to lexical rules for SQL'
 language SQL
 deterministic
 no sql
 sql security invoker
-begin    
+begin
     declare v_length int unsigned default character_length(p_text);
     declare v_no_ansi_quotes        bool default find_in_set('ANSI_QUOTES', @@sql_mode) = FALSE;
     declare v_char, v_lookahead, v_quote_char    char(1) charset utf8;
     declare v_from int unsigned;
     declare allow_script_tokens tinyint unsigned;
-    
+
     set allow_script_tokens := (language_mode = 'script');
 
     if p_from is null then
@@ -91,31 +90,33 @@ begin
         set p_level = p_level - 1;
     end if;
     set v_from = p_from;
-    
+
     set p_token = ''
     ,   p_state = 'start';
+
     my_loop: while v_from <= v_length do
         set v_char = substr(p_text, v_from, 1)
         ,   v_lookahead = substr(p_text, v_from+1, 1)
         ;
+
         state_case: begin case p_state
-            when 'error' then 
+            when 'error' then
                 set p_from = v_length;
-                leave state_case;            
+                leave state_case;
             when 'start' then
                 case
-                    when v_char between '0' and '9' then 
+                    when v_char between '0' and '9' then
                         set p_state = 'integer';
-                    when v_char between 'A' and 'Z' 
-                    or   v_char between 'a' and 'z' 
+                    when v_char between 'A' and 'Z'
+                    or   v_char between 'a' and 'z'
                     or   v_char = '_' then
-                        set p_state = 'alpha';                        
-                    when v_char = ' ' then 
+                        set p_state = 'alpha';
+                    when v_char = ' ' then
                         set p_state = 'whitespace'
                         ,   v_from = v_length - character_length(ltrim(substring(p_text, v_from)))
                         ;
                         leave state_case;
-                    when v_char in ('\t', '\n', '\r') then 
+                    when v_char in ('\t', '\n', '\r') then
                         set p_state = 'whitespace';
                     when v_char = '''' or v_no_ansi_quotes and v_char = '"' then
                         set p_state = 'string', v_quote_char = v_char;
@@ -171,7 +172,7 @@ begin
                             leave my_loop;
                         end if;
                     when v_char = '-' then
-                        case 
+                        case
                             when v_lookahead = '-' and substr(p_text, v_from + 2, 1) = ' ' then
                                 set p_state = 'single line comment'
                                 ,   v_from = locate('\n', p_text, p_from)
@@ -197,11 +198,11 @@ begin
                     when v_char = '+' then
                         set p_state = 'plus', v_from = v_from + 1;
                         leave my_loop;
-                    when v_char = '<' then 
+                    when v_char = '<' then
                         set p_state = 'less than';
-                    when v_char = '>' then 
+                    when v_char = '>' then
                         set p_state = 'greater than';
-                    when v_char = ':' then 
+                    when v_char = ':' then
                         if v_lookahead = '=' then
                             set p_state = 'assign', v_from = v_from + 2;
                             leave my_loop;
@@ -211,22 +212,22 @@ begin
                             set p_state = 'colon', v_from = v_from + 1;
                             leave my_loop;
                         end if;
-                    when v_char = '{' and allow_script_tokens then 
+                    when v_char = '{' and allow_script_tokens then
                         set p_state = 'left braces', v_from = v_from + 1, p_level = p_level + 1;
                         leave my_loop;
-                    when v_char = '}' and allow_script_tokens then 
+                    when v_char = '}' and allow_script_tokens then
                         set p_state = 'right braces', v_from = v_from + 1;
                         leave my_loop;
-                    when v_char = '(' then 
+                    when v_char = '(' then
                         set p_state = 'left parenthesis', v_from = v_from + 1, p_level = p_level + 1;
                         leave my_loop;
-                    when v_char = ')' then 
+                    when v_char = ')' then
                         set p_state = 'right parenthesis', v_from = v_from + 1;
                         leave my_loop;
-                    when v_char = '^' then 
+                    when v_char = '^' then
                         set p_state = 'bitwise xor', v_from = v_from + 1;
                         leave my_loop;
-                    when v_char = '~' then 
+                    when v_char = '~' then
                         set p_state = 'bitwise not', v_from = v_from + 1;
                         leave my_loop;
                     when v_char = '!' then
@@ -250,34 +251,34 @@ begin
                             set p_state = 'bitwise and', v_from = v_from + 1;
                         end if;
                         leave my_loop;
-                    else 
+                    else
                         set p_state = 'error';
                 end case;
             when 'less than' then
-                case v_char 
-                    when '=' then 
+                case v_char
+                    when '=' then
                         set p_state = 'less than or equals';
                         leave state_case;
-                    when '>' then 
+                    when '>' then
                         set p_state = 'not equals';
-                    when '<' then 
+                    when '<' then
                         set p_state = 'left shift';
                     else
                         do null;
                 end case;
                 leave my_loop;
             when 'less than or equals' then
-                if v_char = '>' then 
+                if v_char = '>' then
                     set p_state = 'null safe equals'
                     ,   v_from = v_from + 1
                     ;
                 end if;
                 leave my_loop;
             when 'greater than' then
-                case v_char 
-                    when '=' then 
+                case v_char
+                    when '=' then
                         set p_state = 'greater than or equals';
-                    when '>' then 
+                    when '>' then
                         set p_state = 'right shift';
                     else
                         set p_state = 'error';
@@ -288,67 +289,111 @@ begin
                     set v_from = v_from + 2;
                     leave my_loop;
                 end if;
-            when 'alpha' then 
+            when 'alpha' then
                 case
-                    when v_char between 'A' and 'Z' 
-                    or   v_char between 'a' and 'z' 
+                    when v_char between 'A' and 'Z'
+                    or   v_char between 'a' and 'z'
                     or   v_char = '_' then
                         leave state_case;
-                    when v_char between '0' and '9' 
-                    or   v_char = '$' then 
+                    when v_char between '0' and '9'
+                    or   v_char = '$' then
                         set p_state = 'alphanum';
                     else
                         leave my_loop;
                 end case;
-            when 'alphanum' then 
+            when 'alphanum' then
                 case
-                    when v_char between 'A' and 'Z' 
-                    or   v_char between 'a' and 'z' 
+                    when v_char between 'A' and 'Z'
+                    or   v_char between 'a' and 'z'
                     or   v_char = '_'
-                    or   v_char between '0' and '9' then 
+                    or   v_char between '0' and '9' then
                         leave state_case;
                     else
                         leave my_loop;
                 end case;
             when 'integer' then
-                case 
-                    when v_char between '0' and '9' then 
+                case
+                    when v_char between '0' and '9' then
                         leave state_case;
-                    when v_char = '.' then 
+                    when v_char = '.' then
                         set p_state = 'decimal';
                     else
-                        leave my_loop;                        
+                        leave my_loop;
                 end case;
             when 'decimal' then
-                case 
-                    when v_char between '0' and '9' then 
+                case
+                    when v_char between '0' and '9' then
                         leave state_case;
                     else
                         leave my_loop;
                 end case;
             when 'whitespace' then
                 if v_char not in ('\t', '\n', '\r') then
-                    leave my_loop;                        
+                    leave my_loop;
                 end if;
             when 'string' then
+                -- find the closing quote
                 set v_from = locate(v_quote_char, p_text, v_from);
-                if v_from then
+                if v_from then  -- found a closing quote
+                    if substr(p_text, v_from - 1, 1) = '\\' then
+                        -- this quote was preceded by a backslash.
+                        -- we now have to figure out if this was an escaping backslash.
+                        backslahses: begin
+                            declare v_backslash int unsigned default v_from - 2;
+                            while substr(p_text, v_backslash, 1) = '\\' do
+                                -- we found 2 consecutive backslashes.
+                                -- see if there are even more:
+                                if substr(p_text, v_backslash - 1, 1) = '\\' then
+                                    -- more backslashes, continue the loop.
+                                    set v_backslash = v_backslash - 2;
+                                else
+                                    -- no more backslases.
+                                    -- The quote was not escaped by a backslash.
+                                    leave backslahses;
+                                end if;
+                            end while;
+                            -- if we arrive here, the backslash escaped the quote.
+                            -- this means we haven't found the end of the string yet.
+                            -- so, we have to look beyond the quote for a new one.
+                            set v_from = v_from + 1;
+                            if v_from > v_length then
+                                set p_state = 'error';
+                                leave my_loop;
+                            else
+                                iterate my_loop;
+                            end if;
+                        end backslahses;
+                    end if;
+
+                    -- by now we established that the quote was not escaped by a preceding backslash.
+                    -- but it could still be escaped by a following quote char.
                     if substr(p_text, v_from + 1, 1) = v_quote_char then
-                        set v_from = v_from + 1;
-                    elseif substr(p_text, v_from - 1, 1) != '\\' then
+                        -- this quote is followed by the same quote,
+                        -- this means it was an escaped quote.
+                        -- so, continue beyond this point to find the real end of the string.
+                        set v_from = v_from + 2;
+                        if v_from > v_length then
+                            set p_state = 'error';
+                            leave my_loop;
+                        else
+                            iterate my_loop;
+                        end if;
+                    else
+                        -- ok, this quote appears to be the real end of the string.
+                        -- leave to produce the string token.
                         set v_from = v_from + 1;
                         leave my_loop;
                     end if;
-                else
-                    set p_state = 'error';
+                else  -- no closing quote found. This must be an error.
+                    set p_state = 'error', v_from = v_length;
                     leave my_loop;
                 end if;
             when 'quoted identifier' then
-                if v_char != v_quote_char then 
+                if v_char != v_quote_char then
                     leave state_case;
                 else
                     set v_from = v_from + 1;
-                    leave my_loop;                
+                    leave my_loop;
                 end if;
             when 'user-defined variable' then
                 if v_char in (';', ',', ' ', '\t', '\n', '\r', '!', '~', '^', '%', '>', '<', ':', '=', '+', '-', '&', '*', '|', '(', ')') then
@@ -375,7 +420,7 @@ begin
                     leave my_loop;
                 end if;
             else
-                leave my_loop;            
+                leave my_loop;
         end case; end state_case;
         set v_from = v_from + 1;
     end while my_loop;
