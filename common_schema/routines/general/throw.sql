d87f7f229cd98dbdeed1d704cbd68a83279d7a3e
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2013-07-25 12:35:31 +0000

    throw() now SIGNALs on MySQL >= 5.5
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@499 c8a82860-ae11-f3b5-05a3-c0c726401f9a

--- common_schema/routines/general/throw.sql
+++ common_schema/routines/general/throw.sql
@@ -12,10 +12,14 @@ language SQL
 deterministic
 no sql
 sql security invoker
-begin
+main_body: begin
   declare error_statement VARCHAR(1500) CHARSET utf8;
 
   set @common_schema_error := error_message;
+  /*!50500
+  SIGNAL SQLSTATE VALUE '91100' SET MESSAGE_TEXT = @common_schema_error;
+   */
+  -- MySQL 5.1 does not support SIGNAL. Fallback:
   set error_statement := CONCAT('SELECT error FROM error.`', error_message, '`');
   call exec_single(error_statement);
 end;
