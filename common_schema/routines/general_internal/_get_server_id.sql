158f14ccbd76a428f5debd68e16132336dd5bcff
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2013-08-01 11:11:54 +0000

    Added missing _get_server_id()
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@509 c8a82860-ae11-f3b5-05a3-c0c726401f9a

---
+++ common_schema/routines/general_internal/_get_server_id.sql
@@ -0,0 +1,16 @@
+-- 
+-- return the @@server_id (to be used from within views)
+-- 
+
+DELIMITER $$
+
+DROP FUNCTION IF EXISTS _get_server_id $$
+CREATE FUNCTION _get_server_id() RETURNS INT UNSIGNED 
+SQL SECURITY INVOKER
+COMMENT 'Return current server id'
+
+BEGIN
+  RETURN @@global.server_id;
+END $$
+
+DELIMITER ;
