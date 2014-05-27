d2e1748d91afa1631fe66f914cef96c885fede5e
shlomi.noach@gmail.com <shlomi.noach@gmail.com@c8a82860-ae11-f3b5-05a3-c0c726401f9a>
2014-05-27 12:49:43 +0000

    fixed issue 61: _as_datetime accepts wrongly interprets some unix timestamps as datetime
    git-svn-id: https://common-schema.googlecode.com/svn/trunk@554 c8a82860-ae11-f3b5-05a3-c0c726401f9a

--- common_schema/routines/time/_as_datetime.sql
+++ common_schema/routines/time/_as_datetime.sql
@@ -12,7 +12,13 @@ SQL SECURITY INVOKER
 COMMENT 'Convert given text to DATETIME or NULL.'
 
 BEGIN
-  declare continue handler for SQLEXCEPTION return NULL; 
+  declare continue handler for SQLEXCEPTION return NULL;
+  -- workaround for Issue 61:
+  -- The _as_datetime() function misbehaves with timestamps the look look like real dates.
+  -- UNIX_TIMESTAMP('2014-05-28') = 1401220800. And _as_datetime('1401220800') = '2014-01-22 08:00:00'.
+  if txt RLIKE '^[0-9]{10}$' then
+    return NULL;
+  end if;
   RETURN (txt + interval 0 second);
 END $$
 
